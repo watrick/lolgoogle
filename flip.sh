882ecf91bbf4edbfe028896548fc231df3e659ac
watrick <pfedigan@WL4837P108318.ad.lhfs.com>
2018-02-15 09:54:20 -0800

    more random

--- flip.sh
+++ flip.sh
@@ -2,17 +2,18 @@ while true
 do
 # flip a coin
 FLIP=$(($(($RANDOM%10))%2))
-
+RAN1=$(($RANDOM%3))
+RAN2=$(($RANDOM%3))
 
 # if heads, do the command 
 if [ $FLIP -eq 1 ]
 then
 # ping google
 ping -c 6 -i .5 google.com | lolcat;
-sleep .9
+sleep $RAN1
 else
 #clear out the text
-sleep .2
+sleep $RAN2
 clear
 fi
 done

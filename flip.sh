18f229122aaa7988ccc446c97e066fe958777de2
watrick <pfedigan@WL4837P108318.ad.lhfs.com>
2018-03-01 14:45:44 -0800

    More Random

--- flip.sh
+++ flip.sh
@@ -4,12 +4,13 @@ do
 FLIP=$(($(($RANDOM%10))%2))
 RAN1=$(($RANDOM%3))
 RAN2=$(($RANDOM%2))
+RAN3=$(($RANDOM%3 + 4))
 
 # if heads, do the command 
 if [ $FLIP -eq 1 ]
 then
 # ping google
-ping -c 6 -i .5 google.com | lolcat;
+ping -c $RAN3 -i .5 google.com | lolcat;
 sleep $RAN1
 else
 #clear out the text
